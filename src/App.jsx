669b153c192573ddf543c37c1f516c5f7309a13a
madjonga <madjonga123@gmail.com>
2023-08-27 21:03:58 +0300

    Add GameBoard component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 import { StartScreen } from './components/startScreen'
+import { GameBoard } from './components/GameBoard'
 
 function App() {
   //const [championNames, setChampionNames] = useState([])
@@ -12,37 +13,18 @@ function App() {
     setNumberOfChampions(number)
   }
 
-
-    // http://ddragon.leagueoflegends.com/cdn/13.16.1/img/champion/Aatrox.png
-
-    // url for loading screens http://ddragon.leagueoflegends.com/cdn/img/champion/loading/Aatrox_0.jpg
-    /*
-    useEffect(() => {
-      async function fetchData() {
-        const response = await fetch('https://ddragon.leagueoflegends.com/cdn/6.24.1/data/en_US/champion.json')
-        const data = await response.json()
-  
-        // Extract champion names from the data
-        const names = Object.keys(data.data)
-        setChampionNames(names)
-        
-      }
-      fetchData()
-    }, [])
-    */
-
-    return (
-      <>
-      <h1>League Of Legends Memory Card Game</h1>
-      {!isPlaying ?
-        <StartScreen 
-          startGame={startGame} 
-        />
-        :
-        <p>Playing... with {numberOfChampions} Champions!</p>
-      }
-      </>
-    )
+  return (
+    <>
+    <h1>League Of Legends Memory Card Game</h1>
+    {!isPlaying ?
+      <StartScreen 
+        startGame={startGame} 
+      />
+      :
+      <GameBoard numberOfChampions={numberOfChampions} />
+    }
+    </>
+  )
 
 }
 
