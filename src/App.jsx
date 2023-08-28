1b38c69b277f1b84bab16333013199c93663ca67
madjonga <madjonga123@gmail.com>
2023-08-28 23:05:02 +0300

    One more fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './styles/App.css'
-import { StartScreen } from './components/startScreen'
+import { StartScreen } from './components/StartScreen'
 import { CardBoard } from './components/CardBoard'
 
 export default function App() {
